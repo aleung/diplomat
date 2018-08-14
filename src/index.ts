cda918fdbe1f869472c8888c1fb16d41ae596c26
David Yao <david.yao@ericsson.com>
2018-08-14 17:23:29 +0800

    add distribute and cirbuit breaker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import {sleep} from './utils';
+import { sleep } from './utils';
+import * as _ from 'lodash';
 
 
 interface RequestCall<T> {
@@ -32,11 +33,101 @@ interface TimeoutOptions {
   maxWait: number;
 }
 
+const enum CircuitBreakerState {
+  CLOSED,
+  OPEN,
+  HALF_OPEN
+}
+
+const CircuitBreakerStateStr = [
+  "CLOSED",
+  "OPEN",
+  "HALF_OPEN"
+]
+
+interface CircuitBreakerOption {
+  failureCountThreshold: number;
+  failureRateThreshold: number;
+  window: number;
+  slots: number;
+  resetTimeout: number;
+}
+
+class CircuitBreakerError extends Error {
+  constructor(public readonly errors: Error[]) {
+    super();
+  }
+}
+
+class CircuitBreakerStatics {
+  private slotIterator = 0;
+  private totals: Array<number> = [];
+  private fails: Array<number> = [];
+  private state = CircuitBreakerState.CLOSED;
+
+  constructor(window: number, private slots: number) {
+    this.resetCounts();
+    setInterval(() => {
+      this.slotIterator = ++this.slotIterator % slots;
+      this.totals[this.slotIterator] = 0;
+      this.fails[this.slotIterator] = 0;
+    }, window / slots);
+  }
+
+  resetCounts(): void {
+    this.totals = _.range(0, this.slots, 0); //[0,0,..0]
+    this.fails = _.range(0, this.slots, 0);  //[0,0,..0]
+  }
+
+  incTotal(): void {
+    this.totals[this.slotIterator]++;
+  }
+
+  incFail(): void {
+    this.fails[this.slotIterator]++;
+  }
+
+  getTotal(): number {
+    return _.sum(this.totals);
+  }
+
+  getFail(): number {
+    return _.sum(this.fails);
+  }
+
+  getState(): CircuitBreakerState {
+    return this.state;
+  }
+
+  setState(newState: CircuitBreakerState): void {
+    console.log(`circuit breaker state: ${CircuitBreakerStateStr[this.state]} ===> ${CircuitBreakerStateStr[newState]}`);
+    this.state = newState;
+  }
+}
+
+interface CircuitBreakerStaticsMapType {
+  [propName: string]: CircuitBreakerStatics
+}
+
+
+interface DistributeOptions {
+  addrs: Array<any>;
+  policy: string;
+  maxAttempt: number;
+  maxWait: number;
+}
+
+class DistributeError extends Error {
+  constructor(public readonly errors: Error[]) {
+    super();
+  }
+}
 
 class Diplomat<T> {
+  private CircuitBreakerStaticsMap: CircuitBreakerStaticsMapType = {};
 
   private chain: Array<ChainCall<T>> = [];
-
+  private currentBackendID: string = '';
   run(fn: RequestCall<T>): RequestCall<T> {
     return this.chain.reduceRight<RequestCall<T>>((f, chainFn) => chainFn(f), fn);
   }
@@ -46,6 +137,115 @@ class Diplomat<T> {
       (next) => (...args) => {
         console.log('-> fallback', ...args);
         return next(...args).catch(() => fn(...args));
+      });
+    return this;
+  }
+
+  distribute(options: DistributeOptions) {
+    this.chain.push(
+      (next) => async (...args) => {
+        console.log('-> distribute', ...args);
+        options.addrs = _.concat([...args], options.addrs);
+        let iterator = 0;
+        let attempt = 0;
+        let timeOut = false;
+        const expire = new Promise<T>((resolve, _reject) => {
+          if (options.maxWait) {
+            setTimeout(() => {
+              console.log('distribute exceed max duration!');
+              timeOut = true;
+              resolve();
+            }, options.maxWait);
+          }
+        });
+
+        while (true) {
+          try {
+            this.currentBackendID=iterator.toString();
+            console.log(`backend id:${this.currentBackendID}`);
+            return await Promise.race([
+              expire,
+              next(options.addrs[iterator])
+            ]);
+          } catch (err) {
+            if (options.maxAttempt && ++attempt >= options.maxAttempt) {
+              console.log('distribute exceed max attempt!');
+              throw new DistributeError([new Error("Distribute exceed max attempt!")]);
+            }
+            if (timeOut) {
+              throw new DistributeError([new Error("Distribute exceed max duration!")]);
+            }
+            if (options.policy === 'ordered') {
+              iterator = (++iterator) % options.addrs.length;
+            } else {
+              iterator = Math.floor(options.addrs.length * Math.random());
+            }
+          }
+          // need to sleep awhile, otherwise timer 
+          // callback get no chance to run
+          await sleep(1);
+        }
+      });
+    return this;
+  }
+
+  circuitBreaker(option: CircuitBreakerOption): Diplomat<T> {
+    let CircuitBreakerStaticsMap = this.CircuitBreakerStaticsMap;
+    let statics: CircuitBreakerStatics; 
+
+    function getMyStatics(id: string): CircuitBreakerStatics {
+      if (!CircuitBreakerStaticsMap[id]) {
+        CircuitBreakerStaticsMap[id] = new CircuitBreakerStatics(option.window, option.slots);
+      }
+      return CircuitBreakerStaticsMap[id];
+    }
+
+    function transitToOpen() {
+      statics.setState(CircuitBreakerState.OPEN);
+      setTimeout(() => {
+        statics.setState(CircuitBreakerState.HALF_OPEN);
+      }, option.resetTimeout);
+    }
+
+    function transitToClose() {
+      statics.setState(CircuitBreakerState.CLOSED);
+      statics.resetCounts();
+    }
+
+    function exceedFailureThreshold(){
+      return (statics.getFail()>=option.failureCountThreshold || statics.getFail()/statics.getTotal()>=option.failureCountThreshold);
+    }
+
+    this.chain.push(
+      (next) => async (...args) => {
+        console.log(`-> circuitBreaker  ${this.currentBackendID} `, ...args);
+        if(this.currentBackendID){
+          statics = getMyStatics(this.currentBackendID);
+        }else{
+          statics = getMyStatics('single');
+        }
+        if(statics.getState() === CircuitBreakerState.OPEN){
+            const msg = `reject due to circuit ${this.currentBackendID} is open!`;
+            console.log(msg);
+            throw new CircuitBreakerError([new Error(msg)]);
+        }
+
+        try {
+          statics.incTotal();
+          let res = await next(...args);
+          if (statics.getState() === CircuitBreakerState.HALF_OPEN) {
+            transitToClose();
+          }
+          return res;
+        } catch (err) {
+          console.log(err);
+          statics.incFail();
+          if (exceedFailureThreshold()) {
+            transitToOpen();
+            throw new CircuitBreakerError([new Error(`circuit is opened!`)]);
+          }
+          throw err;
+        }
       });
     return this;
   }
@@ -104,4 +304,7 @@ function diplomat<T>() {
 
 export {
   diplomat,
+  CircuitBreakerError,
+  CircuitBreakerStatics,
+  DistributeError,
 }
--- test/example.ts
+++ test/example.ts
@@ -1,8 +1,10 @@
 
 import * as request from 'request-promise-native';
 import * as _ from 'lodash';
+import * as sinon from 'sinon';
+import { expect } from 'chai';
 
-import { diplomat } from '../src/';
+import { diplomat, CircuitBreakerError, CircuitBreakerStatics, DistributeError } from '../src/';
 import { sleep } from '../src/utils';
 
 function fallback(host, a, b) {
@@ -23,6 +25,15 @@ function alwaysFail(host, a, b): never {
   throw Error(msg);
 }
 
+function successOrFail(returnSuccess:boolean):Promise<string>{
+  console.log(`-> successOrFail:${returnSuccess}`);
+  if(returnSuccess){
+    return Promise.resolve('Current call is successful')
+  }else{
+    return Promise.reject('Current call is failure')
+  }
+}
+
 async function noReturn(host, a, b) {
   const msg = `Error - ${host}, ${a}, ${b}`;
   console.log('-> noReturn:', msg);
@@ -30,8 +41,159 @@ async function noReturn(host, a, b) {
   return Promise.resolve(msg);
 }
 
-
 describe('diplomat', () => {
+
+  it('distribute: stop trying when until one path is successful', async () => {
+    const spySuccessOrFail = sinon.spy(successOrFail);
+    const f = diplomat().distribute(
+      {
+        addrs: [true, true],
+        policy: 'ordered',
+        maxAttempt: 200000,
+        maxWait: 1000
+      }).run(spySuccessOrFail);
+    try {
+      await f(false);
+      sinon.assert.calledWith(spySuccessOrFail, true);
+      sinon.assert.calledWith(spySuccessOrFail, false);
+      sinon.assert.callCount(spySuccessOrFail, 2);
+    } catch (err) {
+      sinon.assert.fail('exception is thrown!',err);
+    }
+  });
+
+  it('distribute: stop trying when attempt exceed max', async () => {
+    const spyAlwaysFail = sinon.spy(alwaysFail);
+    const maxAttempt = 4;
+    const f = diplomat().distribute(
+      {
+        addrs: ['b.com', 'c.com'],
+        policy: 'ordered',
+        maxAttempt: maxAttempt,
+        maxWait: 1000
+      }).run(spyAlwaysFail);
+    try {
+      await f('a.com');
+      sinon.assert.fail('exception is not thrown!');
+    } catch (err) {
+      expect(err).to.be.an.instanceof(DistributeError);
+      sinon.assert.callCount(spyAlwaysFail, maxAttempt);
+      sinon.assert.calledWith(spyAlwaysFail, 'a.com');
+      sinon.assert.calledWith(spyAlwaysFail, 'b.com');
+      sinon.assert.calledWith(spyAlwaysFail, 'c.com');
+    }
+
+  }).timeout(3000);
+
+  const maxWait = 10;
+  it('distribute: stop trying when duration exceed max', async () => {
+    const spyAlwaysFail = sinon.spy(alwaysFail);
+    const f = diplomat().distribute(
+      {
+        addrs: ['b.com', 'c.com'],
+        policy: 'random',
+        maxAttempt: 200000,
+        maxWait: maxWait
+      }).run(spyAlwaysFail);
+    try {
+      await f('a.com');
+      sinon.assert.fail('exception is not thrown!');
+    } catch (err) {
+      expect(err).to.be.an.instanceof(DistributeError);
+    }
+  }).timeout(30+maxWait);
+
+  it('circuit breaker window should advance', async () => {
+    const slotDuration = 100;
+    const slots = 10;
+    const window = slots * slotDuration;
+    const statics = new CircuitBreakerStatics(window, slots);
+    statics.incTotal();
+    statics.incFail();
+    await sleep(slotDuration * 3);
+    statics.incTotal();
+    statics.incTotal();
+    statics.incFail();
+    expect(statics.getTotal()).to.eql(3);
+    expect(statics.getFail()).to.eql(2);
+    // now window have advanced 1 slot
+    await sleep(slotDuration * 8);
+    expect(statics.getTotal()).to.eql(2);
+    expect(statics.getFail()).to.eql(1);
+  }).timeout(3000);
+
+  it('call should be made when circuit is half open,call should not be made when circuit is open', async () => {
+    const resetTimeout = 1000;
+    const option = {
+      failureCountThreshold: 1,
+      failureRateThreshold: 0.9,
+      window: 10000,
+      slots: 1,
+      resetTimeout: resetTimeout
+    }
+    const spyAlwaysFail = sinon.spy(alwaysFail);
+    const spyAlwaysSuccess = sinon.spy(alwaysSuccess);
+    const f = diplomat().circuitBreaker(option).run(spyAlwaysFail);
+
+    await f('foo.com').catch(() => { });
+    sinon.assert.callCount(spyAlwaysFail, 1);
+
+    // now circuit is open, immediate subsequent call should be skipped
+    try {
+      await f('foo.com');
+      sinon.assert.fail('exception is not thrown!');
+    } catch (err) {
+      expect(err).to.be.an.instanceof(CircuitBreakerError);
+    }
+    sinon.assert.callCount(spyAlwaysFail, 1);
+
+    await sleep(resetTimeout + 200);
+    // now circuit is half open, test call should be made
+    await f('foo.com').catch(() => { });
+    sinon.assert.callCount(spyAlwaysFail, 2);
+
+    // now circuit is opened again as last call fail, immediate subsequent call should be skipped
+    await f('foo.com').catch(() => { });
+    sinon.assert.callCount(spyAlwaysFail, 2);
+
+    await sleep(resetTimeout + 200);
+    // now circuit is half open, test call should be made
+    await diplomat().circuitBreaker(option).run(spyAlwaysSuccess)()
+    sinon.assert.callCount(spyAlwaysSuccess, 1);
+
+    // now circuit is closed again as last call is successful, subsequent call should be made
+    await diplomat().circuitBreaker(option).run(spyAlwaysSuccess)()
+    sinon.assert.callCount(spyAlwaysSuccess, 2);
+
+  }).timeout(15000);
+
+  it('distribute().circuitBreaker() trigger circuit break per address', async () => {
+    const resetTimeout = 1000;
+    const spyAlwaysFail = sinon.spy(alwaysFail);
+    const f = diplomat()
+    .distribute({
+      addrs: ['b.com', 'c.com'],
+      policy: 'ordered',
+      maxAttempt: 5,
+      maxWait: 10*1000  
+    })
+    .circuitBreaker({
+      failureCountThreshold: 1,
+      failureRateThreshold: 0.9,
+      window: 1000,
+      slots: 1,
+      resetTimeout: resetTimeout
+    })
+    .run(spyAlwaysFail);
+
+    try {
+      await f('a.com');
+      sinon.assert.fail('exception is not thrown!');
+    } catch (err) {
+      expect(err).to.be.an.instanceof(DistributeError);
+    }
+    sinon.assert.callCount(spyAlwaysFail, 3);
+  }).timeout(15000);
 
   it('always success', async () => {
     const f = diplomat()
